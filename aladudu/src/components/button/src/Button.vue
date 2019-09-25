e20304d223193c099488ed5df09733b3ae17d4de
徐阿姿 <xuazi_vendor@sensetime.com>
2019-09-25 16:22:28 +0800

    feat (Button组件) 初始化组件样式

---
+++ aladudu/src/components/button/src/Button.vue
@@ -0,0 +1,24 @@
+<template>
+    <div id="button" class="middle" @click="btnEvent()">
+        {{btnMessage}}
+    </div>
+</template>
+<script>
+import '../../../style/components/button.css'
+export default {
+    data() {
+        return {
+            btnMessage: "确定"
+        }
+    },
+    methods: {
+        btnEvent() {
+            alert("点击")
+        }
+    }
+}
+</script>
+
+
+
+
--- aladudu/src/router/index.js
+++ aladudu/src/router/index.js
@@ -1,15 +1,16 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-// import HelloWorld from '@/components/HelloWorld'
+import Button from '../components/button/src/Button.vue'
+
 
 Vue.use(Router)
 
 export default new Router({
   routes: [
     {
-      // path: '/',
-      // name: 'HelloWorld',
-      // component: HelloWorld
+      path: '/',
+      name: 'Button',
+      component: Button
     }
   ]
 })
